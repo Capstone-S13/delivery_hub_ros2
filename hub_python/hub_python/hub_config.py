7ae66b4c54540763833b7aa4119abcc0c38e95ce
lkw303 <lkw303@gmail.com>
2022-06-21 23:10:54 +0800

    hub_node tested with collection and delivery operations
    Signed-off-by: lkw303 <lkw303@gmail.com>

--- hub_python/hub_python/hub_config.py
+++ hub_python/hub_python/hub_config.py
@@ -1,13 +1,23 @@
 
 class HubConfig():
     def __init__(self):
-        self.slider_rec_pos = (None,None, None)
-        self.slider_send_pos = (None, None, None)
+        self.slider_rec_pos = (45.0,0.0, 445.0)
+        self.slider_send_pos = (365.0, 0.0, 300.0)
 
         # positions for the toolhead to collect from pigeon holes
-        self.toolhead_collect_pos = []
+        self.toolhead_collect_pos = [
+            # (45.0,0.0,670.0),
+            (350.0,0.0,680.0),
+            # (45.0,0.0,800.0),
+            # (365.0,0.0,800.0)
+        ]
 
         # positions for the toolhead to deposit from pigeon holes
         # this positions should be have a greater z value than the
         # corresponding values in collect_pos
-        self.toolhead_deposit_pos = []
+        self.toolhead_deposit_pos = [
+            # (45.0,0.0,615.0),
+            (350.0,0.0,615.0),
+            # (45.0,0.0,750.0),
+            # (365.0,0.0,750.0)
+        ]
