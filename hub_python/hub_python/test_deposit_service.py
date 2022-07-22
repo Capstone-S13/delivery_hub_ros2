94fe015b45db80f248b4b44dea898e859001c087
lkw303 <lkw303@gmail.com>
2022-07-22 23:13:25 +0800

    change test scripts to take in arguments
    Signed-off-by: lkw303 <lkw303@gmail.com>

--- hub_python/hub_python/test_deposit_service.py
+++ hub_python/hub_python/test_deposit_service.py
@@ -1,30 +1,35 @@
 import rclpy
+import sys
+import argparse
 from rclpy.node import Node
 from hub_interfaces.srv import HubService
 from hub_interfaces.msg import HubOperation
-import uuid
 
 
 class TestHubServiceClient(Node):
-    def __init__(self):
+    def __init__(self, argv=sys.argv):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-c', '--company_name', required=True,
+                            type=str, help='company name of order')
+        parser.add_argument('-o', '--order_id', required=True,
+                            type=str, help='order id')
+        self.args = parser.parse_args(argv[1:])
         super().__init__('test_hub_service_client')
         self.service_client = self.create_client(HubService, 'hub_service')
         while not self.service_client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info("waiting for hub service server")
 
-    def send_request(self,company_name, order_id):
+    def send_request(self):
         request = HubService.Request()
         request.operation.operation = HubOperation.OPERATION_DEPOSIT
-        request.company_name = company_name
-        request.order_id = order_id
+        request.company_name = self.args.company_name
+        request.order_id = self.args.order_id
         self.future  = self.service_client.call_async(request)
 
 def main():
-    company_name = "barg"
-    order_id = company_name + "my_uid2"
     rclpy.init()
     client_node = TestHubServiceClient()
-    client_node.send_request(company_name, order_id)
+    client_node.send_request()
     while rclpy.ok():
         rclpy.spin_once(client_node)
         if client_node.future.done():
